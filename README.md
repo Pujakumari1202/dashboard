a0d3d2db728d86f2ade073e6a9b46b7a19dbabd1
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-11-12 00:54:42 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 ## Dashboard using Streamlit python
+
+
+
+## Steps to run the project 
+
+````bash
+  streamlit run dashboard.py
+
+````
+
+````bash
+  streamlit run dashboard.py --server.port 8080
+
+````
+
+````bash
+  pip install -r requirements.txt
+````
---
+++ dashboard.py
@@ -0,0 +1,29 @@
+# import some libraries
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# set the title of the page
+st.set_page_config(page_title="Superstore!!!!", page_icon=":bar_chart:",layout="wide")
+
+
+st.title(" :bar_chart: Sample SuperStore EDA")
+
+# add some padding to move this title up
+st.markdown('<style>dive.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
+
+
+#load the file
+f1=st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls"]))
+if f1 is not None:
+    filename=f1.name
+    st.write(filename)
+    df= pd.read_csv(filename,encoding="ISO-8859-1")
+
+else:
+    os.chdir(r"C:\Users\PUJA KUMARI\Desktop\dashboard")
+    df=pd.read_csv("Superstore.csv",encoding="ISO-8859-1")  
