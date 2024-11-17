534c21552a2d55617fa229da40f6346f610e3726
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-11-17 12:00:11 +0530

    commit

--- dashboard.py
+++ dashboard.py
@@ -22,10 +22,42 @@ f1=st.file_uploader(":file_folder: Upload a file",type=(["csv","txt","xlsx","xls
 if f1 is not None:
     filename=f1.name
     st.write(filename)
-    df= pd.read_csv(filename,encoding="ISO-8859-1")
+    df= pd.read_csv(filename,encoding="ISO-8859-1",delim_whitespace=True, on_bad_lines="skip")
 
 else:
     os.chdir(r"C:\Users\PUJA KUMARI\Desktop\dashboard")
-    df=pd.read_csv("Superstore.csv",encoding="ISO-8859-1")  
+    df=pd.read_csv("Superstore.csv",encoding="ISO-8859-1",delim_whitespace=True, on_bad_lines="skip")
 
-    
+
+#create a date picker ,user can choose specific period of the data
+col1, col2 = st.columns((2)) # create two columns
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+
+
+# Getting the min and max date
+startDate = pd.to_datetime(df["Order Date"]).min()
+endDate = pd.to_datetime(df["Order Date"]).max()
+
+# taking the input  of the date
+with col1:
+    date1 = pd.to_datetime(st.date_input("Start Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("End Date", endDate))
+
+
+# based on data our dataframe data will be updated
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+st.sidebar.header("Choose your filter: ")
+# Create for Region
+region = st.sidebar.multiselect("Pick your Region", df["Region"].unique())
+
+#if we are not selecting any region(E,W,N,S)
+if not region:
+    df2=df.copy() #df store in df2
+else :
+    df2=df[df["Region"].isin(region)]
+
+# Create for state if we select any bthen we get only that part of states
